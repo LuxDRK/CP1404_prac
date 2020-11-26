edd67dc9113c8e8f64ee336d2ea412619ba7f654
LuxDRK <xray728@gmail.com>
2020-11-26 12:55:38 +0800

    initial commit by lux

---
+++ prac_02/randoms.py
@@ -0,0 +1,22 @@
+"""
+What did you see on line 1?
+What was the smallest number you could have seen, what was the largest?
+Integer Number from 5 to 20
+Smallest is 5 and largest is 20
+
+What did you see on line 2?
+What was the smallest number you could have seen, what was the largest?
+Could line 2 have produced a 4?
+Odd number from 3 to 9
+Smallest is 3 and largest is 9
+Can't be 4
+
+What did you see on line 3?
+What was the smallest number you could have seen, what was the largest?
+Real number from 2.5 to 5.5
+Smallest is 2.5 and largest is 5.499999999
+
+Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+"""
+import random
+print(random.randrange(1,100))
